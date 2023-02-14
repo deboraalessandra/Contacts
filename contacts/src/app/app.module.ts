65fca3f3208637eddf6622140b577d0d7dc217d4
Débora Rocha <debora.rocha@iatec.com>
2023-02-14 16:53:58 -0300

    Module contact created

--- contacts/src/app/app.module.ts
+++ contacts/src/app/app.module.ts
@@ -7,18 +7,27 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { ToolbarComponent } from './toolbar/toolbar.component';
+import { ListagemComponent } from './listagem/listagem.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { ContactModule } from './contact/contact.module';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ToolbarComponent
+    ToolbarComponent,
+    ListagemComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    MatIconModule
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ContactModule
   ],
   providers: [],
   bootstrap: [AppComponent]
