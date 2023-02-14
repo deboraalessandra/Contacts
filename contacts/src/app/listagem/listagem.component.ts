418408ef9331309538f3e9c871fd47fb6859f108
Débora Rocha <debora.rocha@iatec.com>
2023-02-14 16:54:22 -0300

    Component listagem created

---
+++ contacts/src/app/listagem/listagem.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listagem',
+  templateUrl: './listagem.component.html',
+  styleUrls: ['./listagem.component.scss']
+})
+export class ListagemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
