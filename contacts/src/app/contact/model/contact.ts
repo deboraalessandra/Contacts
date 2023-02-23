91ce63a9a82659fc667508d785464adf7dc02f76
Débora Rocha <debora.rocha@iatec.com>
2023-02-23 13:33:47 -0300

    Created model

---
+++ contacts/src/app/contact/model/contact.ts
@@ -0,0 +1,13 @@
+export interface Contact {
+   id?: number,
+   nome?: string,
+   sobrenome?: string,
+   email?: string,
+   telefone?: number
+
+}
+//Esse módulo será usado para compartilhamento
+//Foi criado um contrutor já com os atributos, para ele criar automaticamente
+// o ? significa que eles são opcionais na chamada do construtor
+
+
