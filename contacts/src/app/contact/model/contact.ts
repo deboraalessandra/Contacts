20f79f82d1d969629b09022cb639ff2508b6d870
Débora Rocha <debora.rocha@iatec.com>
2023-02-28 14:13:18 -0300

    Model contacts

--- contacts/src/app/contact/model/contact.ts
+++ contacts/src/app/contact/model/contact.ts
@@ -1,10 +1,9 @@
 export interface Contact {
-   id?: number,
-   nome?: string,
-   sobrenome?: string,
-   email?: string,
-   telefone?: number
-
+   id: number,
+   name: string,
+   lastName: string,
+   email: string,
+   phone: number
 }
 //Esse módulo será usado para compartilhamento
 //Foi criado um contrutor já com os atributos, para ele criar automaticamente
