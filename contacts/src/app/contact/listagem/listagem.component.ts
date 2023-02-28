451618e35b9ad5bfd3b63b6bd41c69b2db050486
Débora Rocha <debora.rocha@iatec.com>
2023-02-28 14:21:59 -0300

    Component Listagem

---
+++ contacts/src/app/contact/listagem/listagem.component.ts
@@ -0,0 +1,25 @@
+import { SolutionService } from './../services/solution.service';
+import { Component, OnInit } from '@angular/core';
+
+import { Contact } from '../model/contact';
+import { ContactService } from '../services/contact.service';
+
+@Component({
+  selector: 'app-listagem',
+  templateUrl: './listagem.component.html',
+  styleUrls: ['./listagem.component.scss']
+})
+export class ListagemComponent implements OnInit {
+  contacts: Contact[] = [
+  ]; // lista de atributo de pessoas
+  displayedColumns = ['id', 'name', 'lastName', 'email', 'phone'];
+
+  constructor(private SoluctionService: SolutionService) {} //adicionando no construtor a injeção de serviço
+
+  ngOnInit(): void {
+    //  this.SoluctionService.listarTodos().subscribe(console.log);
+    this.SoluctionService.listarTodos()
+      .subscribe(dados => this.contacts = dados);
+  }
+
+}
