4f26e18c754f1325d10f65a50787e2199cd1e503
Débora Rocha <debora.rocha@iatec.com>
2023-03-07 14:01:45 -0300

    Spinner

--- contacts/src/app/contact/listagem/listagem.component.ts
+++ contacts/src/app/contact/listagem/listagem.component.ts
@@ -11,16 +11,18 @@ import { SolutionService } from './../services/solution.service';
 })
 export class ListagemComponent implements OnInit {
   displayedColumns = ['id', 'name', 'lastName', 'email', 'phone', 'remove', 'edit'];
-  contacts: Contact[] = []; // lista de atributo de pessoas
+  //contacts: Contact[] = []; // lista de atributo de pessoas
 
   contacts$: Observable<Contact[]>;
 
-  constructor(private SoluctionService: SolutionService) {} //adicionando no construtor a injeção de serviço
+  constructor(private SoluctionService: SolutionService) {
+    this.contacts$ = this.SoluctionService.listarTodos();
+  } //adicionando no construtor a injeção de serviço
 
   ngOnInit(): void {
     //  this.SoluctionService.listarTodos().subscribe(console.log);
-     this.SoluctionService.listarTodos()
-      .subscribe(dados => this.contacts = dados);
+    //  this.SoluctionService.listarTodos()
+    //   .subscribe(dados => this.contacts = dados);
     // this.contacts$ = this.SoluctionService.listarTodos();
   }
 
