35739e52f1c61e60bf6380e1876ddd2865a6a2b4
Débora Rocha <debora.rocha@iatec.com>
2023-03-15 09:22:00 -0300

    Service edit conclued

--- contacts/src/app/contact/services/solution.service.ts
+++ contacts/src/app/contact/services/solution.service.ts
@@ -15,21 +15,25 @@ export class SolutionService {
   //private readonly API = 'http://localhost:3000/contacts';
 
   constructor(private httpCliente: HttpClient) //injeção de dependências
-  {}
+  { }
 
   listarTodos() {
     return this.httpCliente.get<Contact[]>(this.API)
       .pipe(
         delay(1000)
-        // tap(contacts => console.log(contacts))
       );
   }
 
-  save(contact: Contact){
+  save(contact: Contact) {
     return this.httpCliente.post<Contact[]>(this.API, contact); //retorna um observable de curso
   }
 
   delete(contact: Contact) {
     return this.httpCliente.delete(`${this.API}/${contact.id}`);
+  }
+
+  edit(contact: Contact) {
+    const url = `${this.API}/${contact.id}`;
+    return this.httpCliente.put<Contact[]>(url, contact);
   }
 }
